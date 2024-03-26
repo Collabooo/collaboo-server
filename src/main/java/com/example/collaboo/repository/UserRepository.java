825fabdb3471754a603cd138f8d68b2506916802
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-27 00:01:30 +0900

    refactor : 로그인 api 완성

--- src/main/java/com/example/collaboo/repository/UserRepository.java
+++ src/main/java/com/example/collaboo/repository/UserRepository.java
@@ -3,5 +3,7 @@ import com.example.collaboo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Custom queries or methods if needed
+    User findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
