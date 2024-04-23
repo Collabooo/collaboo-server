be722af5a0b7b445bd76fb784dec6a2d0dd61072
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-04-23 10:01:38 +0900

    error : cors에러 해결

---
+++ src/main/java/com/example/collaboo/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.example.collaboo.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@Service
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
